e372e4e0817668599505c2d0e0a07c38cc8d45a7
Frosty-WinPC <al.korobko@yahoo.com>
2026-02-24 20:34:35 +0100

    add: new mvn module common in core add: Spring Boot, Spring Cloud, PostgreSQL and Lombok dependencies and plugins to menu module

---
+++ core/menu/src/main/java/at/frosty/restaurant/menu/MenuApp.java
@@ -0,0 +1,11 @@
+package at.frosty.restaurant.menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MenuApp.class);
+    }
+}
