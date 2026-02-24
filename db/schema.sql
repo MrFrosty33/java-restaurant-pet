a25bf175d7c9bf47c5efef18ef3186b1e92dc167
Frosty-WinPC <al.korobko@yahoo.com>
2026-02-24 20:55:24 +0100

    add: db schema.sql with menu_dishes database including tables dishes & dish_allergens

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE menu_dishes;
+
+\connect menu_dishes;
+
+CREATE TABLE IF NOT EXISTS dishes (
+    id UUID PRIMARY KEY,
+    name VARCHAR(150),
+    price NUMERIC(5, 2),
+    category VARCHAR(100),
+    --todo categories
+    --CONSTRAINT check_category CHECK (category IN ('')),
+    CONSTRAINT check_price CHECK (price > 0)
+);
+
+CREATE TABLE IF NOT EXISTS dish_allergens (
+    dish_id UUID REFERENCES dishes(id),
+    allergen CHAR NOT NULL,
+    PRIMARY KEY (dish_id, allergen)
+);
