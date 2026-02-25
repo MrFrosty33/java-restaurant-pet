0a99805a2573ef10405272761fe3d1eb46492d80
Frosty-WinPC <al.korobko@yahoo.com>
2026-02-25 13:19:22 +0100

    add: MenuApp

--- core/menu/src/main/java/at/frosty/restaurant/menu/MenuApp.java
+++ core/menu/src/main/java/at/frosty/restaurant/menu/MenuApp.java
@@ -2,8 +2,10 @@ package at.frosty.restaurant.menu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"at.frosty.restaurant.menu", "at.frosty.restaurant.common.model.menu"})
+@EntityScan(basePackages = "at.frosty.restaurant.common.model.menu")
 public class MenuApp {
     public static void main(String[] args) {
         SpringApplication.run(MenuApp.class);
