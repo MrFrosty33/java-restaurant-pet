e5516a60834160e53e61d25350ca215f3310e6eb
Frosty-WinPC <al.korobko@yahoo.com>
2026-02-25 13:20:34 +0100

    add: basic SecurityConfig and RestaurantUserDetailsService create and configure

---
+++ core/menu/src/main/java/at/frosty/restaurant/menu/configuration/SecurityConfig.java
@@ -0,0 +1,46 @@
+package at.frosty.restaurant.menu.configuration;
+
+import at.frosty.restaurant.menu.service.RestaurantUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final RestaurantUserDetailsService restaurantUserDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/menu/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/menu/**").permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authManager() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(restaurantUserDetailsService);
+        authProvider.setPasswordEncoder(getPasswordEncoder());
+
+        return new ProviderManager(authProvider);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ core/menu/src/main/java/at/frosty/restaurant/menu/service/RestaurantUserDetailsService.java
@@ -0,0 +1,41 @@
+package at.frosty.restaurant.menu.service;
+
+import at.frosty.restaurant.common.model.menu.RestaurantUser;
+import at.frosty.restaurant.menu.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class RestaurantUserDetailsService implements UserDetailsService {
+    private final String className = this.getClass().getSimpleName();
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        RestaurantUser restaurantUser = userRepository.findByUsername(username)
+                .orElseThrow(() -> {
+                    log.warn("{}: unable to find user with username={}", className, username);
+                    return new UsernameNotFoundException("User with username=" + username + " does not exist");
+                });
+
+
+        UserDetails result = User.builder()
+                .username(restaurantUser.getUsername())
+                .password(restaurantUser.getPassword())
+                .authorities(restaurantUser.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority(role.name()))
+                        .toList())
+                .build();
+
+        log.info("{}: result of loadUserByUsername(username={}): {}", className, username, result);
+        return result;
+    }
+}
