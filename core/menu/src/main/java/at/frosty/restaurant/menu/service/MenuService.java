4f2d265d20e772e7496097f5df157950d3c2a5c1
Frosty-WinPC <al.korobko@yahoo.com>
2026-03-04 19:30:19 +0100

    add: Manager methods to MenuService interface

--- core/menu/src/main/java/at/frosty/restaurant/menu/service/MenuService.java
+++ core/menu/src/main/java/at/frosty/restaurant/menu/service/MenuService.java
@@ -2,6 +2,7 @@ package at.frosty.restaurant.menu.service;
 
 import at.frosty.restaurant.common.model.menu.Category;
 import at.frosty.restaurant.common.model.menu.dto.DishDto;
+import at.frosty.restaurant.common.model.menu.dto.UpdateDishDto;
 import at.frosty.restaurant.menu.enums.DishSearchType;
 import at.frosty.restaurant.menu.enums.DishSortType;
 
@@ -9,8 +10,14 @@ import java.util.List;
 import java.util.UUID;
 
 public interface MenuService {
+    // public methods
     DishDto getByUuid(UUID uuid);
     List<DishDto> getAllSorted(DishSortType sortBy);
     List<DishDto> getByCategory(Category category, DishSortType sortBy);
     List<DishDto> search(String query, DishSearchType type);
+    // admin methods
+    List<DishDto> getDeactivatedDishes();
+    DishDto createDish(DishDto dishDto);
+    DishDto updateDish(UUID uuid, UpdateDishDto updateDishDto);
+    void softDeleteDish(UUID uuid);
 }
