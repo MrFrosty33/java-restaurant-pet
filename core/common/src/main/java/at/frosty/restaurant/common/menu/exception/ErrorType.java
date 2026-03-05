701d601c235c58994c056cbb88c4ffc7b39c19c9
Frosty-WinPC <al.korobko@yahoo.com>
2026-03-05 16:02:50 +0100

    add: new ErrorType

--- core/common/src/main/java/at/frosty/restaurant/common/menu/exception/ErrorType.java
+++ core/common/src/main/java/at/frosty/restaurant/common/menu/exception/ErrorType.java
@@ -2,6 +2,7 @@ package at.frosty.restaurant.common.menu.exception;
 
 public enum ErrorType {
     DISH_NOT_FOUND,
+    DISH_DEACTIVATED,
     UNAUTHORIZED,
     VALIDATION_ERROR,
     INTERNAL_ERROR;
