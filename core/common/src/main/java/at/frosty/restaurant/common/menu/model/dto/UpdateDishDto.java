388ad35f96360adcd4cb6cc11dfb5d512ad68e6f
Frosty-WinPC <al.korobko@yahoo.com>
2026-03-05 17:49:36 +0100

    add: data lombok annotation

--- core/common/src/main/java/at/frosty/restaurant/common/menu/model/dto/UpdateDishDto.java
+++ core/common/src/main/java/at/frosty/restaurant/common/menu/model/dto/UpdateDishDto.java
@@ -4,11 +4,13 @@ import at.frosty.restaurant.common.menu.model.Allergen;
 import at.frosty.restaurant.common.menu.model.Category;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
 import java.math.BigDecimal;
 import java.util.Set;
 
+@Data
 public class UpdateDishDto {
     @Length(max = 150)
     private String name;
