9d59172827d4c0bbffbd001d12b2945da348c7ea
Frosty-WinPC <al.korobko@yahoo.com>
2026-03-05 18:15:12 +0100

    fix: packages names after rename

--- core/menu/src/main/java/at/frosty/restaurant/menu/MenuApp.java
+++ core/menu/src/main/java/at/frosty/restaurant/menu/MenuApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication(scanBasePackages = {"at.frosty.restaurant.menu", "at.frosty.restaurant.common.model.menu"})
-@EntityScan(basePackages = "at.frosty.restaurant.common.model.menu")
+@SpringBootApplication(scanBasePackages = {"at.frosty.restaurant.menu", "at.frosty.restaurant.common.menu"})
+@EntityScan(basePackages = "at.frosty.restaurant.common.menu.model")
 public class MenuApp {
     public static void main(String[] args) {
         SpringApplication.run(MenuApp.class);
