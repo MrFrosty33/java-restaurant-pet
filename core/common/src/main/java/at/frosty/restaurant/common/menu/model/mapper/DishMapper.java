c3103c7adc3a8b185ce258a767f0d7935ce84f1a
Frosty-WinPC <al.korobko@yahoo.com>
2026-03-05 18:13:47 +0100

    add: updateDishFromUpdateDto()

--- core/common/src/main/java/at/frosty/restaurant/common/menu/model/mapper/DishMapper.java
+++ core/common/src/main/java/at/frosty/restaurant/common/menu/model/mapper/DishMapper.java
@@ -2,8 +2,12 @@ package at.frosty.restaurant.common.menu.model.mapper;
 
 import at.frosty.restaurant.common.menu.model.Dish;
 import at.frosty.restaurant.common.menu.model.dto.DishDto;
+import at.frosty.restaurant.common.menu.model.dto.UpdateDishDto;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface DishMapper {
@@ -11,4 +15,7 @@ public interface DishMapper {
     Dish toEntity(DishDto dto);
 
     DishDto toDto(Dish entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateDishFromUpdateDto(UpdateDishDto dto, @MappingTarget Dish entity);
 }
