fd41409943640befcae3e767c21f400debeb81bc
Frosty-WinPC <al.korobko@yahoo.com>
2026-05-06 17:54:21 +0200

    improve: disable csrf and make session stateless

--- core/menu/src/main/java/at/frosty/restaurant/menu/configuration/SecurityConfig.java
+++ core/menu/src/main/java/at/frosty/restaurant/menu/configuration/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,10 +24,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authenticationProvider(authenticationProvider())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/actuator/**").hasRole("OBSERVER")
-                        .requestMatchers("/management/menu/**").hasRole("MANAGER")
+                        .requestMatchers("/management/**").hasRole("MANAGER")
                         .requestMatchers("/menu/**").permitAll()
                         .anyRequest().authenticated()
                 )
