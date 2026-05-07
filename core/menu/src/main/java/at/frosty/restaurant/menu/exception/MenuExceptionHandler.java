c873ac8d2036f20a3f46b9ff3dfdff090f199b2d
Frosty-WinPC <al.korobko@yahoo.com>
2026-05-07 18:48:17 +0200

    add: new exception for better logging and response

--- core/menu/src/main/java/at/frosty/restaurant/menu/exception/MenuExceptionHandler.java
+++ core/menu/src/main/java/at/frosty/restaurant/menu/exception/MenuExceptionHandler.java
@@ -7,8 +7,11 @@ import at.frosty.restaurant.common.menu.exception.ForbiddenException;
 import at.frosty.restaurant.common.menu.exception.IncludesErrorType;
 import at.frosty.restaurant.common.menu.exception.NotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -59,6 +62,45 @@ public class MenuExceptionHandler {
                 .build();
     }
 
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorMessage handleMissingServletRequestParameter(MissingServletRequestParameterException e, HttpServletRequest request) {
+        writeLog(e);
+        return ErrorMessage.builder()
+                .message(e.getMessage())
+                .errorType(ErrorType.VALIDATION_ERROR)
+                .status(HttpStatus.BAD_REQUEST)
+                .apiPath(request.getRequestURI())
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorMessage handleConstraintViolation(ConstraintViolationException e, HttpServletRequest request) {
+        writeLog(e);
+        return ErrorMessage.builder()
+                .message(e.getMessage())
+                .errorType(ErrorType.VALIDATION_ERROR)
+                .status(HttpStatus.BAD_REQUEST)
+                .apiPath(request.getRequestURI())
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorMessage handleMethodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+        writeLog(e);
+        return ErrorMessage.builder()
+                .message(e.getMessage())
+                .errorType(ErrorType.VALIDATION_ERROR)
+                .status(HttpStatus.BAD_REQUEST)
+                .apiPath(request.getRequestURI())
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorMessage handleOthers(Exception e, HttpServletRequest request) {
@@ -74,7 +116,10 @@ public class MenuExceptionHandler {
 
     private void writeLog(Exception ex) {
         ErrorType errorType;
-        if(ex instanceof IncludesErrorType) errorType = ((IncludesErrorType) ex).getErrorType();
+        if (ex instanceof IncludesErrorType) errorType = ((IncludesErrorType) ex).getErrorType();
+        else if (ex instanceof MissingServletRequestParameterException) errorType = ErrorType.VALIDATION_ERROR;
+        else if (ex instanceof ConstraintViolationException) errorType = ErrorType.VALIDATION_ERROR;
+        else if (ex instanceof MethodArgumentNotValidException) errorType = ErrorType.VALIDATION_ERROR;
         else errorType = ErrorType.INTERNAL_ERROR;
 
         log.warn("{}: caught {} with errorType={}. message={}", className,
