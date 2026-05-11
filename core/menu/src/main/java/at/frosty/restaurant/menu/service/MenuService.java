0af00af564694fadb7d8d12c9abab7ed889d4564
Frosty-WinPC <al.korobko@yahoo.com>
2026-05-11 17:53:55 +0200

    add: implementation of InternalController getDishOrderByUuid() method

--- core/menu/src/main/java/at/frosty/restaurant/menu/service/MenuService.java
+++ core/menu/src/main/java/at/frosty/restaurant/menu/service/MenuService.java
@@ -2,6 +2,7 @@ package at.frosty.restaurant.menu.service;
 
 import at.frosty.restaurant.common.menu.model.Category;
 import at.frosty.restaurant.common.menu.model.dto.DishDto;
+import at.frosty.restaurant.common.menu.model.dto.DishOrderDto;
 import at.frosty.restaurant.common.menu.model.dto.UpdateDishDto;
 import at.frosty.restaurant.menu.enums.DishSearchType;
 import at.frosty.restaurant.menu.enums.DishSortType;
@@ -23,4 +24,7 @@ public interface MenuService {
     DishDto updateDish(UUID uuid, UpdateDishDto updateDishDto);
     DishDto activateDish(UUID uuid);
     void softDeleteDish(UUID uuid);
+
+    //internal methods
+    DishOrderDto getDishOrderByUuid(UUID uuid);
 }
