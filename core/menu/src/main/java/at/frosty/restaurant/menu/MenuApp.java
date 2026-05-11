091720dfef665c934e9c61675b28b893f68fb4c7
Frosty-WinPC <al.korobko@yahoo.com>
2026-05-11 16:48:41 +0200

    add: feign dependencies

--- core/menu/src/main/java/at/frosty/restaurant/menu/MenuApp.java
+++ core/menu/src/main/java/at/frosty/restaurant/menu/MenuApp.java
@@ -3,9 +3,11 @@ package at.frosty.restaurant.menu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {"at.frosty.restaurant.menu", "at.frosty.restaurant.common.menu"})
 @EntityScan(basePackages = "at.frosty.restaurant.common.menu.model")
+@EnableFeignClients(basePackages = "at.frosty.restaurant.common.menu.feign")
 public class MenuApp {
     public static void main(String[] args) {
         SpringApplication.run(MenuApp.class);
