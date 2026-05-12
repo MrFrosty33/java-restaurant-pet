fe13442188002bcd8a3ca54e6fa083dbf10c7765
Frosty-WinPC <al.korobko@yahoo.com>
2026-05-12 17:21:49 +0200

    refactor: RestaurantUser, RestaurantUserDetailsService and UserRepository moved to common module

--- core/common/src/main/java/at/frosty/restaurant/common/security/model/RestaurantUser.java
+++ core/common/src/main/java/at/frosty/restaurant/common/security/model/RestaurantUser.java
@@ -1,5 +1,6 @@
-package at.frosty.restaurant.common.menu.model;
+package at.frosty.restaurant.common.security.model;
 
+import at.frosty.restaurant.common.menu.model.Role;
 import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
--- core/menu/src/main/java/at/frosty/restaurant/menu/MenuApp.java
+++ core/menu/src/main/java/at/frosty/restaurant/menu/MenuApp.java
@@ -3,10 +3,12 @@ package at.frosty.restaurant.menu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"at.frosty.restaurant.menu", "at.frosty.restaurant.common.menu"})
-@EntityScan(basePackages = "at.frosty.restaurant.common.menu.model")
+@SpringBootApplication(scanBasePackages = {"at.frosty.restaurant.menu", "at.frosty.restaurant.common.menu",
+        "at.frosty.restaurant.common.security"})
+@EnableJpaRepositories(basePackages = {"at.frosty.restaurant.menu.repository", "at.frosty.restaurant.common.security.repository"})
+@EntityScan(basePackages = {"at.frosty.restaurant.common.menu.model", "at.frosty.restaurant.common.security.model"})
 public class MenuApp {
     public static void main(String[] args) {
         SpringApplication.run(MenuApp.class);
