cf619334119e0439f72fab76a984f3869f72fd96
Frosty-WinPC <al.korobko@yahoo.com>
2026-05-13 22:43:23 +0200

    add: service-auth-credentials config for menu-service and it's class

--- core/menu/src/main/java/at/frosty/restaurant/menu/MenuApp.java
+++ core/menu/src/main/java/at/frosty/restaurant/menu/MenuApp.java
@@ -3,14 +3,16 @@ package at.frosty.restaurant.menu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"at.frosty.restaurant.menu", "at.frosty.restaurant.common.menu",
-        "at.frosty.restaurant.common.security"})
+        "at.frosty.restaurant.common.feign", "at.frosty.restaurant.common.security"})
 @EnableJpaRepositories(basePackages = {"at.frosty.restaurant.menu.repository", "at.frosty.restaurant.common.security.repository"})
 @EntityScan(basePackages = {"at.frosty.restaurant.common.menu.model", "at.frosty.restaurant.common.security.model"})
-@EnableFeignClients(basePackages = {"at.frosty.restaurant.common.security.feign"})
+@EnableFeignClients(basePackages = {"at.frosty.restaurant.common.feign"})
+@ConfigurationPropertiesScan(basePackages = {"at.frosty.restaurant.common.feign.config"})
 public class MenuApp {
     public static void main(String[] args) {
         SpringApplication.run(MenuApp.class);
