1b472314cbd417642c78179665de93e1708a6299
Frosty-WinPC <al.korobko@yahoo.com>
2026-05-13 22:44:00 +0200

    add: FeignAuthConfig

---
+++ common/src/main/java/at/frosty/restaurant/common/feign/config/FeignAuthConfig.java
@@ -0,0 +1,33 @@
+package at.frosty.restaurant.common.feign.config;
+
+import feign.RequestInterceptor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class FeignAuthConfig {
+    private final String className = this.getClass().getSimpleName();
+    private final ServiceAuthCredentials serviceAuthCredentials;
+
+    @Bean
+    public RequestInterceptor basicAuthRequestInterceptor() {
+        return requestTemplate -> {
+            log.info("{}: found serviceAuthCredentials: {}", className, serviceAuthCredentials);
+            String encoded = Base64.getEncoder()
+                    .encodeToString(serviceAuthCredentials.getCredentials().getBytes(StandardCharsets.UTF_8));
+
+            requestTemplate.header(
+                    HttpHeaders.AUTHORIZATION,
+                    "Basic " + encoded
+            );
+        };
+    }
+}
