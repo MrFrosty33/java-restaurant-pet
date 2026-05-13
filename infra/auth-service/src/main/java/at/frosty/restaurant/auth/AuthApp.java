2e8d0fa161bf9fc5d9f3eccdfd6ecdf4ee2949dd
Frosty-WinPC <al.korobko@yahoo.com>
2026-05-13 18:08:56 +0200

    add: AuthApp

---
+++ infra/auth-service/src/main/java/at/frosty/restaurant/auth/AuthApp.java
@@ -0,0 +1,13 @@
+package at.frosty.restaurant.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication(scanBasePackages = {"at.frosty.restaurant.auth", "at.frosty.restaurant.common.security"})
+@EntityScan(basePackages = {"at.frosty.restaurant.common.security.model"})
+public class AuthApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApp.class);
+    }
+}
