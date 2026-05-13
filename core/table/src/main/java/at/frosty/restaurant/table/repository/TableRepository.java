92ebde81797d6e4c0efca05d34167e08b2eb89e6
Frosty-WinPC <al.korobko@yahoo.com>
2026-05-14 00:33:48 +0200

    fix: repository method naming

--- core/table/src/main/java/at/frosty/restaurant/table/repository/TableRepository.java
+++ core/table/src/main/java/at/frosty/restaurant/table/repository/TableRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 import java.util.UUID;
 
 public interface TableRepository extends JpaRepository<Table, UUID> {
-    List<Table> findByCapacityGreaterThanEquals(int capacity);
+    List<Table> findByCapacityGreaterThanEqual(int capacity);
 }
