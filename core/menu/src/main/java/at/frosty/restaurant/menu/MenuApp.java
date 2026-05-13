928d1d5a6613476dbe27e51db4ed404cc3cb33eb
Frosty-WinPC <al.korobko@yahoo.com>
2026-05-13 18:22:56 +0200

    add: '@EnableFeignClients' to MenuApp

--- core/menu/src/main/java/at/frosty/restaurant/menu/MenuApp.java
+++ core/menu/src/main/java/at/frosty/restaurant/menu/MenuApp.java
@@ -3,12 +3,14 @@ package at.frosty.restaurant.menu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"at.frosty.restaurant.menu", "at.frosty.restaurant.common.menu",
         "at.frosty.restaurant.common.security"})
 @EnableJpaRepositories(basePackages = {"at.frosty.restaurant.menu.repository", "at.frosty.restaurant.common.security.repository"})
 @EntityScan(basePackages = {"at.frosty.restaurant.common.menu.model", "at.frosty.restaurant.common.security.model"})
+@EnableFeignClients(basePackages = {"at.frosty.restaurant.common.security.feign"})
 public class MenuApp {
     public static void main(String[] args) {
         SpringApplication.run(MenuApp.class);
