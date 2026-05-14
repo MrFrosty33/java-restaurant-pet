d7b85a9e8a8cc59aa1e1d3a9ce2fa5eeaa5a3d25
Frosty-WinPC <al.korobko@yahoo.com>
2026-05-14 15:58:12 +0200

    add: existsByNumber() method to TableRepository

--- core/table/src/main/java/at/frosty/restaurant/table/repository/TableRepository.java
+++ core/table/src/main/java/at/frosty/restaurant/table/repository/TableRepository.java
@@ -4,8 +4,10 @@ import at.frosty.restaurant.common.table.model.Table;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TableRepository extends JpaRepository<Table, UUID> {
     List<Table> findByCapacityGreaterThanEqual(int capacity);
+    boolean existsByNumber(int number);
 }
