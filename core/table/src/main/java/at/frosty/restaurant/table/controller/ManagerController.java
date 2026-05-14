f265b858c74e62c239fc11653d489fd6f4b6a555
Frosty-WinPC <al.korobko@yahoo.com>
2026-05-14 18:44:12 +0200

    fix: deleteTable() meant to use '@PathVariable'

--- core/table/src/main/java/at/frosty/restaurant/table/controller/ManagerController.java
+++ core/table/src/main/java/at/frosty/restaurant/table/controller/ManagerController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -65,7 +64,7 @@ public class ManagerController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteTable(@RequestParam UUID id) {
+    public void deleteTable(@PathVariable @NotNull UUID id) {
         log.info("{}: received deleteTable(id={}) call", className, id);
         service.deleteTable(id);
     }
